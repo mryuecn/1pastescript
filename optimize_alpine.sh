93c587a4fe0b58772fa353308f5c4fbe978774ed
mryuecn <mryuecn@163.com>
2024-09-29 21:51:58 +0800

    Update optimize_alpine.sh

--- optimize_alpine.sh
+++ optimize_alpine.sh
@@ -6,18 +6,24 @@ apk update && apk upgrade
 
 # 安装必要软件
 echo "正在安装必要软件..."
-apk add nano curl htop vim openntpd openssh
+apk add nano curl htop vim openntpd
 
 # 配置时区为北京时间
 echo "正在设置时区为北京时间..."
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "Asia/Shanghai" > /etc/timezone
 
+# 配置vm.swappiness
+echo "正在配置vm.swappiness..."
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+sysctl -p
+
 # 配置SSH
 echo "正在配置SSH..."
 sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config
-rc-service sshd start  # 启动SSH服务
+rc-update add sshd default  # 添加sshd服务到默认运行级别
+rc-service sshd start       # 启动sshd服务
 
 # 启用时间同步服务
 echo "正在启用时间同步服务..."
